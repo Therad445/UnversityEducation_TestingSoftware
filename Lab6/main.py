19f2092d5266138e7f0e369149e697bd6ec23641
Radmir <islamov.radmir2014@yanddex.ru>
2023-11-25 03:16:47 +0300

    Edit Lab 6

--- Lab6/main.py
+++ Lab6/main.py
@@ -24,7 +24,11 @@ def prime_factors(n):
 def main():
     pool = multiprocessing.Pool()
     results = pool.map(prime_factors, range(1, 101))
-    print(results)
+    for i in range(len(results)):
+        str_out = ""
+        for elem in results[i]:
+            str_out = str_out + " " + str(elem)
+        print(str(i) + " = " + str_out)
 
 
 if __name__ == '__main__':
